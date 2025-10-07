805b6f515925c3c9913bd61533afc7fcff7e4f3d
Yuliya Reshetnikova <Reshetnikova_Y@reshetnikova-mb.local>
2025-10-07 12:29:38 +0300

    baseTheme

---
+++ src/lib/theme/styled.d.ts
@@ -0,0 +1,191 @@
+import "styled-components"
+
+declare module "styled-components" {
+    export interface DefaultTheme {
+   colors: {
+    bg: {
+        disabled: string;
+        marine: {
+            calm: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            loud: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            loudStatic: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+        }
+          red: {
+            calm: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            loud: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            loudStatic: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+        }
+          blue: {
+            calm: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            loud: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            loudStatic: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+        }
+        lead: {
+            calm: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            loud: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            loudStatic: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+        }
+        neutral: {
+            section: {
+                calm: string;
+                calmInverted: string;
+                bold: string;
+                lightStatic: string;
+                darkStatic: string;
+            }
+            global: {
+                color: string;
+            }
+            
+        }
+    }
+    text: {
+      blue: {
+            calm: {
+                enabled: string;
+            }
+            loud: {
+                enabled: string;
+            }
+        }  
+        marine: {
+            calm: {
+                enabled: string;
+            }
+            loud: {
+                enabled: string;
+            }
+        } 
+        red: {
+            calm: {
+                enabled: string;
+            }
+            loud: {
+                enabled: string;
+            }
+        } 
+        neutral: {
+            calm: {
+                enabled: string;
+            }
+            loud: {
+                enabled: string;
+            }
+            inverted: {
+                enabled: string;
+            }
+        } 
+    }
+    border: {
+        lead: {
+            calm: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            loud: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            onLight: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            onDark: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+            inverted: {
+                enabled: string;
+                hover: string;
+                active: string;
+            }
+        }  
+        marine: {
+            calm: {
+                enabled: string;
+            }
+            loud: {
+                enabled: string;
+            }
+        } 
+        blue: {
+            calm: {
+                enabled: string;
+            }
+            loud: {
+                enabled: string;
+            }
+        }
+        red: {
+            calm: {
+                enabled: string;
+            }
+            loud: {
+                enabled: string;
+            }
+        }
+        neutral: {
+            calm: {
+                disabled: string;
+            }
+            loud: {
+                disabled: string;
+            }
+        }
+
+    }
+   }     
+    }
+}
